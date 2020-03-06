0f16586f0bfb31594e7f09c332ad7e91b82c5451
Lemuel Monteiro <48529602+lemuelmm@users.noreply.github.com>
2020-03-06 17:16:25 -0300

    evilCorp adicionada

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import homeWeb from './img/Home.png';
 import loginMobile from './img/Login.jpg';
 import solicitacoes from './img/Solicitações.jpg';
 import local from './img/Local.jpg';
-import avaliacao from './img/Avaliação.jpg';
 import impacto from './img/Impacto.jpg';
 import perfil from './img/Solicitações-2.jpg';
 import enviado from './img/Solicitações-1.jpg';
@@ -15,12 +14,19 @@ import avalExpand from './img/Avaliação-1.jpg';
 
 //web Imgs
 import pesqusiadores from './img/webImg/Pesquisadores-1.jpg';
-import geositios from './img/webImg/Geosítios.jpg';
 import solicitacoesWeb from './img/webImg/Solicitações-1.jpg';
 import geositioMain from './img/webImg/Relatório Geosítio.jpg';
 import geositio1 from './img/webImg/Relatório Geosítio-1.jpg';
 import geopark from './img/webImg/Relatório Geopark.jpg';
 import geopark1 from './img/webImg/Relatório Geopark-1.jpg';
+
+//Evo Corp Site
+import home from './img/evoImg/Home.jpg';
+import sevices from './img/evoImg/Home-1.jpg';
+import locals from './img/evoImg/Home-3.jpg';
+import about from './img/evoImg/Home-5.jpg';
+import msg1 from './img/evoImg/Home-2.jpg';
+import msg2 from './img/evoImg/Home-4.jpg';
 
 
 function App() {
@@ -67,8 +73,9 @@ function App() {
         <h3>Telas Geopark Web:</h3>
           <div id="flex">
             <img id="webMainContent" src={homeWeb} alt=""/>
-            <p className="mainMobile">A aplicação Web tem como usuário o administrador, e será onde os relatórios serão apresentados, e o gerenciamento dos pesquisadores, geosítios e solicitações serão feitos</p>
+            <p className="mainMobile">A aplicação Web tem como usuário o administrador, e será onde os relatórios dos geosítios e geoparques serão apresentados, além do gerenciamento dos pesquisadores cadastrados, geosítios e solicitações de relatórios de geosítios que deverão ser feitos.</p>
           </div>
+        <div>
           <div className="webPopup">
             <Popup trigger={<img id="webContent" src={pesqusiadores} alt=""/>} modal closeOnDocumentClick position="left center">
               <div className="containerImg">
@@ -107,6 +114,54 @@ function App() {
               </div>
             </Popup>
           </div>
+        </div>
+
+
+      </div>
+
+      <h2>Site Institucional</h2>
+      <div>
+        <div className="webPopup">
+          <Popup trigger={<img id="webContent" src={home} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={home} alt=""/>
+            </div>
+          </Popup>
+          <Popup trigger={<img id="webContent" src={sevices} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={sevices} alt=""/>
+            </div>
+          </Popup>
+        </div>
+
+        <div className="webPopup">
+          <Popup trigger={<img id="webContent" src={locals} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={locals} alt=""/>
+            </div>
+          </Popup>
+          <Popup trigger={<img id="webContent" src={about} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={about} alt=""/>
+            </div>
+          </Popup>
+        </div>
+
+        <div className="webPopup">
+          <Popup trigger={<img id="webContent" src={msg1} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={msg1} alt=""/>
+            </div>
+          </Popup>
+          <Popup trigger={<img id="webContent" src={msg2} alt=""/>} modal closeOnDocumentClick position="left center">
+            <div className="containerImg">
+              <img className="imgWebExpand" src={msg2} alt=""/>
+            </div>
+          </Popup>
+          <p>* esse site não está em no ar, foi feito unico e exclusivamente para representar minhas capacidades de refazer um site real</p>
+        </div>
+       
+
 
       </div>
     
