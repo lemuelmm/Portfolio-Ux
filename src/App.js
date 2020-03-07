c53ebaed3eb195cfb4573873408b49d4a57a7b3e
Lemuel Monteiro <48529602+lemuelmm@users.noreply.github.com>
2020-03-06 21:47:11 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -160,7 +160,8 @@ function App() {
           </Popup>
         </div>
           <p>* esse site não está em no ar, foi feito unico e exclusivamente para representar minhas capacidades de refazer um site real</p>
-          <p>Para o projeto do Geopark Araripe foi usado a plataforma de prototipação Figma, para criar o site institucional eu usei o Adobe Xd</p>
+        <p></>      
+    <p className="adendo">Para o projeto do Geopark Araripe foi usado a plataforma de prototipação Figma, para criar o site institucional eu usei o Adobe Xd</p>
        
 
 
